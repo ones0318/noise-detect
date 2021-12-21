39c6e5fc27e7de4e12a85e31b836897767a5560b
Yeh Shuyung <>
2021-12-21 23:42:59 +0800

    fix

--- src/poc.py
+++ src/poc.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import sys
 import time
 import queue
 
@@ -8,33 +9,37 @@ import soundfile as sf
 import numpy as np
 
 
-q = queue.Queue(maxsize=1*1024*1024)
+q = queue.Queue(maxsize=512)
 
 record = False
 start = time.time()
 
+
 logging.basicConfig(
     filename=f'{datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.log',
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s|%(name)s|%(levelname)s|%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 
 def print_sound(indata, outdata, frames, time_, status):
     volume_norm = np.linalg.norm(indata)*10
+    if q.full():
+        q.get()
     q.put(indata.copy())
 
     global record
     global start
 
-    threshold = 1
+    threshold = 2
     if volume_norm > threshold and not record:
         start = time.time()
-        logging.info(f'detect sound!! threshold={threshold}')
+        logging.info(f'detect so   und!! threshold={threshold}')
+        print('!!!detect!!!')
         record = True
     if record:
-        if time.time() - start > 10:
+        if time.time() - start > 5:
             with sf.SoundFile(f'records/{datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.wav', mode='w', samplerate=44100, channels=2) as fd:
                 while not q.empty():
                     fd.write(q.get())
@@ -42,5 +47,7 @@ def print_sound(indata, outdata, frames, time_, status):
     print("|" * int(volume_norm))
 
 
-with sd.Stream(callback=print_sound):
-    sd.sleep(900*1000)
+if __name__ == '__main__':
+    time.sleep(900)
+    with sd.Stream(callback=print_sound, blocksize=1024):
+        sd.sleep(6*3600*1000)
